25deb305ca97ff6bac4ea15cf86adf8c5746aba9
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-01-19 07:17:22 +0530

    heroku server for ip change and request

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-import express from './server';
+import express from 'express';
+import bodyParser from 'body-parser';
 
+const app = express();
+app.use(bodyParser.json());
+
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+let serverIp = 'localhost';
+
+app.get('/', (req, res) => {
+    res.redirect(`http://${serverIp}`);
+});
+
+app.post('/changehostip', (req, res) => {
+    serverIp = req.body.ip;
+    console.log(serverIp)
+    res.send("ok");
+});
+
+require('dotenv').config();
+const APP_PORT = process.env.PORT || (process.env.APP_PORT || 3000);
+
+app.set('port',  APP_PORT);
+app.set('host',  process.env.APP_HOST || '0.0.0.0');
+
+
+app.listen(app.get('port'), app.get('host'), () => {
+    console.log(`Server running at http://${app.get('host')}:${app.get('port')}`);
+});
--- server/eventBus.js
+++
@@ -1,7 +0,0 @@
-const eventBus = require('js-event-bus')();
-
-eventBus.on('uncaughtException', function (err) {
-    console.error(err);
-});
-
-export default eventBus;
